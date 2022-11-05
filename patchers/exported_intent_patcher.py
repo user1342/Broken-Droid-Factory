00597242ba556230d4e0afb9be7b328010962ec7
James Stevenson <28961606+user1342@users.noreply.github.com>
2022-11-05 23:48:38 +0000

    Update exported_intent_patcher.py

--- patchers/exported_intent_patcher.py
+++ patchers/exported_intent_patcher.py
@@ -1,5 +1,4 @@
 import os.path
-import random
 import re
 
 from patchers import patcher_interface
@@ -17,12 +16,14 @@ class exported_intent_patcher(patcher_interface.patcher):
         '''
 
         self.logger("Removing an intent filter from an exported activity")
-        path_to_android_manifest = self._get_path_to_file("AndroidManifest.xml", os.path.join(self.working_dir,"app","src","main"))
+        path_to_android_manifest = self._get_path_to_file("AndroidManifest.xml",
+                                                          os.path.join(self.working_dir, "app", "src", "main"))
         manifest_file = open(path_to_android_manifest, "r")
         manifest_file_data = manifest_file.read()
         manifest_file.close()
 
-        manifest_file_data = re.sub(r'(android:exported="true">(((.|\n)*)<\/intent-filter>))','android:exported="true">', manifest_file_data)
+        manifest_file_data = re.sub(r'(android:exported="true">(((.|\n)*)<\/intent-filter>))',
+                                    'android:exported="true">', manifest_file_data)
 
         manifest_file = open(path_to_android_manifest, "w")
         manifest_file.write(manifest_file_data)
